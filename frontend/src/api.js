0bbba696b6a6eee15593320129aa709b2e0c127c
VinutnaBoyapati <vinnu2005@email.com>
2025-06-17 20:23:39 +0530

    Fix baseURL syntax in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    //baseURL: 'http://localhost:5000/api/canvas',
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: 'https://canvabuilder.onrender.com/api/canvas',
 });
 
 export default API;
